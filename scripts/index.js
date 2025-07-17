1ba23dd65ca0b9191c8748cddd4e978d0c23a723
GFSL <glennssc@gmail.com>
2025-07-16 18:42:55 -0600

    display none

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,9 @@ const buttonopen = document.querySelector(".main__button_add");
 const buttonclose = document.querySelector(".popup__button_close");
 const popup = document.querySelector(".popup");
 buttonopen.addEventListener("click", function () {
-  popup.classList.add("popup_open");
+  popup.classList.add("popup_opened");
 });
 
 buttonclose.addEventListener("click", function () {
-  popup.classList.remove("popup_open");
+  popup.classList.remove("popup_opened");
 });
